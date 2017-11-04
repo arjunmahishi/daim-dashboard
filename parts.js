3bf6df244ff030d8b85c3f926f7eca434e95a9ef
Abdulkadir98 <ak98.narutokun@gmail.com>
2017-11-04 18:17:29 +0530

    Displaying parts under different shop types

--- parts.js
+++ parts.js
@@ -1,5 +1,6 @@
-var url = "https://daimler-backend.herokuapp.com/parts/";
-var token = "e7cf60af4daa0ccfbafefc40328085cdc856211b";
+var url = "https://daimler-backend.herokuapp.com/critical_list/";
+var token = "99f2d9e404f270fb5ec6033b0e5fbaa5d8c09f35";
+var partList = [];
 $(function(){
 fetch(url, {
 	 method: "get",
@@ -12,6 +13,21 @@ fetch(url, {
             		response.json().then(function(json){
 					          console.log(json);
                     addCards(json);
+
+                    var i = 0;
+                    for(var k in json){
+                        var value = json[k];
+                        console.log(value);
+                        var dates = Object.keys(value);
+                        partList = json[k][dates[0]]['parts'];
+                        console.log(partList);
+
+                        //method to add individual parts under a part type
+                        addItems(partList,i);
+                        i++;
+
+                    }
+
                     updateDisplay(json);
             	   });
             	}
@@ -20,52 +36,128 @@ fetch(url, {
             	}
             });
 
-function updateDisplay(parts){
+function updateDisplay(json){
+var container = document.querySelector('div.container');
+var children = container.children;
+var i=0;
+for(var k in json){
+  var obj = json[k];
+  var dates = Object.keys(obj);
+  parts = json[k][dates[0]]['parts'];
 
-//Now new list items have been added from addCards method.
-var partContainer = document.querySelector('.collapsible.popout');
+
+
+var partType = children[i].childNodes[1].childNodes[1].childNodes[1]; //card title
+partType.innerHTML = k;
+var partContainer = children[i].childNodes[1].childNodes[1].childNodes[3].childNodes[1]; //#ul class-collapsible popout
 var partDetails = partContainer.children;
 
-console.log(partDetails);
+for(var j=0; j<partDetails.length; j++){
 
 
-for(var i=0; i<partDetails.length; i++) {
+   if(parts.length>0){
 
-    // traverse the DOM and get the li element
-    var supplierNameListItem = partDetails[i].childNodes[1].childNodes[1].childNodes[0];
+    //traverse the DOM and get the li element
+    var supplierNameListItem = partDetails[j].childNodes[1].childNodes[2].childNodes[1];
     var supplierNameHTMLString = supplierNameListItem.innerHTML;
-    supplierNameListItem.innerHTML = supplierNameHTMLString + " " + parts[i].supplier_name;
-    var partNoListItem = partDetails[i].childNodes[1].childNodes[1].childNodes[2];
+    supplierNameListItem.innerHTML = supplierNameHTMLString + " " + parts[j].supplier_name;
+    var partNoListItem = partDetails[j].childNodes[1].childNodes[2].childNodes[3];
     var partNoHTMLString = partNoListItem.innerHTML;
-    partNoListItem.innerHTML = partNoHTMLString + " " + (i+1);
+    partNoListItem.innerHTML = partNoHTMLString + " " + (j+1);
 
-    //traverse the DOM and get the span element on popout
-    var detailSpan = partDetails[i].childNodes[3].childNodes[1];
-    detailSpan.innerHTML = "backlog: " + parts[i].backlog + "<br/>" + "count: " + parts[i].count
-                           + "<br/>" + "description: "  + parts[i].description + "<br/>" +
-                           "ETA: " + parts[i].eta_dicv + "<br/>" +
-                           "planned vehicle quantity: " + parts[i].planned_vehicle_qty + "<br/>" +
-                           "pmc: " + parts[i].pmc + "<br/>" +
-                           "quantity: " + parts[i].quantity + "<br/>" +
-                           "quantity expected: " + parts[i].quantity_expected + "<br/>" +
-                           "reported on: " + parts[i].reported_on + "<br/>" +
-                           "short on: " + parts[i].short_on + "<br/>" +
-                           "shortage reason: " + parts[i].shortage_reason + "<br/>" +
-                           "truck details: " + parts[i].truck_details + "<br/>" +
-                           "unloading point: " + parts[i].unloading_point + "<br/>";
+     //traverse the DOM and get the span element on popout
+    var detailSpan = partDetails[j].childNodes[3].childNodes[0];
+    detailSpan.innerHTML = "backlog: " + parts[j].backlog + "<br/>" + "count: " + parts[j].count
+                           + "<br/>" + "description: "  + parts[j].description + "<br/>" +
+                           "ETA: " + parts[j].eta_dicv + "<br/>" +
+                           "planned vehicle quantity: " + parts[j].planned_vehicle_qty + "<br/>" +
+                           "pmc: " + parts[j].pmc + "<br/>" +
+                           "quantity: " + parts[j].quantity + "<br/>" +
+                           "quantity expected: " + parts[j].quantity_expected + "<br/>" +
+                           "reported on: " + parts[j].reported_on + "<br/>" +
+                           "short on: " + parts[j].short_on + "<br/>" +
+                           "shortage reason: " + parts[j].shortage_reason + "<br/>" +
+                           "truck details: " + parts[j].truck_details + "<br/>" +
+                           "unloading point: " + parts[j].unloading_point + "<br/>";
+   }
 
 }
 
+i++;
+
 }
-function addCards(list){
-    var cardContainer = document.querySelector('.collapsible.popout');
-    var children = cardContainer.childNodes;
-    var card = children[1];
-    for( i=0; i<list.length - 1; i++)
-    {
-        var newItem = card.cloneNode(true);
-        cardContainer.appendChild(newItem);
-    }
+
+
+
+//Now new list items have been added from addCards method.
+// var partContainer = document.querySelector('.collapsible.popout');
+// var partDetails = partContainer.children;
+
+// console.log(partDetails);
+
+
+// for(var i=0; i<partDetails.length; i++) {
+
+//     // traverse the DOM and get the li element
+//     var supplierNameListItem = partDetails[i].childNodes[1].childNodes[2].childNodes[1];
+//     var supplierNameHTMLString = supplierNameListItem.innerHTML;
+//     supplierNameListItem.innerHTML = supplierNameHTMLString + " " + parts[i].supplier_name;
+//     var partNoListItem = partDetails[i].childNodes[1].childNodes[2].childNodes[3];
+//     var partNoHTMLString = partNoListItem.innerHTML;
+//     partNoListItem.innerHTML = partNoHTMLString + " " + (i+1);
+
+//     //traverse the DOM and get the span element on popout
+//     var detailSpan = partDetails[i].childNodes[3].childNodes[0];
+//     detailSpan.innerHTML = "backlog: " + parts[i].backlog + "<br/>" + "count: " + parts[i].count
+//                            + "<br/>" + "description: "  + parts[i].description + "<br/>" +
+//                            "ETA: " + parts[i].eta_dicv + "<br/>" +
+//                            "planned vehicle quantity: " + parts[i].planned_vehicle_qty + "<br/>" +
+//                            "pmc: " + parts[i].pmc + "<br/>" +
+//                            "quantity: " + parts[i].quantity + "<br/>" +
+//                            "quantity expected: " + parts[i].quantity_expected + "<br/>" +
+//                            "reported on: " + parts[i].reported_on + "<br/>" +
+//                            "short on: " + parts[i].short_on + "<br/>" +
+//                            "shortage reason: " + parts[i].shortage_reason + "<br/>" +
+//                            "truck details: " + parts[i].truck_details + "<br/>" +
+//                            "unloading point: " + parts[i].unloading_point + "<br/>";
+
+//                     }
+}
+
+
+function addItems(list,i){
+    var container = document.querySelector('div.container');
+    var children = container.children;
+
+      var itemContainer = children[i].childNodes[1].childNodes[1].childNodes[3].childNodes[1];
+      var child = itemContainer.childNodes;
+      var item = child[1];
+      for( var j=0; j<list.length-1; j++)
+      {
+          var newItem = item.cloneNode(true);
+          itemContainer.appendChild(newItem);
+      }
+
+
+
+}
+
+function addCards(json){
+var cardContainer = document.querySelector('div.container');
+var children = cardContainer.childNodes;
+var card = children[1];
+for(var i=0; i< (Object.keys(json).length)-1; i++){
+    var newCard = card.cloneNode(true);
+    //console.log(newCard);
+
+    cardContainer.appendChild(newCard);
+
+    //collapsibles do not work on dynamic changes and need to be updated each time content changes
+    $('.collapsible').collapsible({
+        accordion : true
+        });
+  }
+  console.log(cardContainer.children);
 
 
 }
@@ -73,10 +165,4 @@ function addCards(list){
 
 });
 
-// var ul = document.getElementById("foo");
-// var items = ul.getElementsByTagName("li");
-// for (var i = 0; i < items.length; ++i) {
-//   // do something with items[i], which is a <li> element
-// }
 
-//Name:<br/>Open stock:<br/>Qty expected:<br/>ETA:
