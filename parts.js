89456893d3f688bf7f6b4432743fdceb80ecaf34
Abdulkadir98 <ak98.narutokun@gmail.com>
2017-11-12 21:32:23 +0530

    displaying parts according to date selection and updated api

--- parts.js
+++ parts.js
@@ -1,79 +1,131 @@
-var url = "https://daimler-backend.herokuapp.com/critical_list/";
-var token = sessionStorage.tokenid || "3d35519e0f437d19e8f625c143bb63a7989753a8";
+var url = "https://daimler-backend.herokuapp.com/parts/?ordering=short_on,-status&shop=";
+var token = "3d35519e0f437d19e8f625c143bb63a7989753a8";
 console.log(token);
 var json;
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1;
+var yyyy = today.getFullYear();
 
-var selection = sessionStorage.selection;
+//global variable that holds a reference to the node that displays parts
+var item;
+
+if(dd<10) {
+    dd = '0'+dd
+}
+
+if(mm<10) {
+    mm = '0'+mm
+}
+
+
+
+var selection = "MDT ENGINE";
+var date = today;
 selection = selection.replace(/ /g,'%20');
+
+date = yyyy + '-' + mm + '-' + dd;
 //if(token==undefined) window.location="http://localhost:3000";
-var partList = [];
 $(function(){
-fetch(url + selection, {
-	 method: "get",
+//For storing a reference to the node that displays parts
+      var container = document.querySelector('div.container');
+      var children = container.children;
+      var itemContainer = children[0].childNodes[1].childNodes[1].childNodes[3].childNodes[1];
+      var child = itemContainer.childNodes;
+      item = child[1];
+
+  //get response from api
+  getData();
+
+  var star = document.getElementById('star');
+star.onclick = function(e){
+var image = e.target;
+this.src = "images/filled_star.png";
+};
+
+// to select a date
+// $('#date-selection').click(function(){
+
+//   var datesCard = document.getElementById('date-choices');
+//   datesCard.style.removeProperty('display');
+//   console.log(datesCard.children);
+
+//   $('#apply-btn').click(function(){
+//       datesCard.setAttribute('style','display: none;');
+
+//       var dateSelected = $('input[name=dates]:checked').next().text();
+//       console.log(dateSelected);
+//       updateDisplay(json, dateSelected);
+
+//   });
+
+// });
+
+
+$('#next').click(function(){
+  var d= $('#date').text();
+  dd= parseInt(d.substr(8,9));
+  dd++;
+  if(dd==31) dd=1;
+  date = yyyy + '-' + mm + '-' + dd;
+
+  getData();
+});
+$('#prev').click(function(){
+  var d= $('#date').text();
+  dd= parseInt(d.substr(8,9));
+
+  dd--;
+  if(dd==0) dd=31;
+  date = yyyy + '-' + mm + '-' + dd;
+  getData();
+});
+
+
+});
+
+function getData(){
+fetch(url + selection+"&short_on="+date, {
+   method: "get",
                 headers: {
                     'Content-Type' : 'application/x-www-form-urlencoded',
                     'Authorization' : 'Token '+token
                 }
             }).then(function(response){
-            	if(response.ok){
-            		response.json().then(function(data){
+              if(response.ok){
+                response.json().then(function(data){
                     json = data;
-					          console.log(json);
-                    updateDates(json);
-                    //addCards(json);
+                    console.log(json);
+                    //updateDates();
+
+                    // var dates = Object.keys(json);
+                    //     for(i in dates){
 
 
-                    // for(var k in json){
-                    //     var value = json[k];
-                    //     console.log(value);
-                        var dates = Object.keys(json);
-                        partList = json[dates[0]]['parts'];
-                        console.log(partList);
+                    //     partList[i] = json[dates[i]];
+                    //     console.log(partList.length);
+                    //      }
 
                         //method to add individual parts under a part type
-                        for(var i=0; i<partList.length - 1; i++){
-                          addItems(partList);
-                        }
 
 
-
-                    // }
-
-                    updateDisplay(json, dates[0]);
-            	   });
-            	}
-            	else {
-            		 console.log('Network request failed with response ' + response.status + ': ' + response.statusText);
-            	}
+                          addItems(json);
+                          updateDisplay(json, date);
+                 });
+              }
+              else {
+                 console.log('Network request failed with response ' + response.status + ': ' + response.statusText);
+              }
             });
+        }
 
-function updateDisplay(json, dateSelected){
-
-var dates = Object.keys(json);
-if(dates.indexOf(dateSelected) === 0){
-  $('#day').text('Today');
-  document.getElementById('date').innerHTML = dateSelected;
-
-}else if(dates.indexOf(dateSelected) === 1){
-    $('#day').text('Tomorrow');
-    $('#date').text(dateSelected);
-
-
-}else {
-      $('#day').text('Day after Tomorrow');
-      $('#date').text(dateSelected);
-
-
-}
-
+function updateDisplay(json, date){
+$('#date').text(date);
 
 var container = document.querySelector('div.container');
 var children = container.children;
 
-
-//for every key in the root object
-
-  parts = json[dateSelected]['parts'];
+var parts = json;
 
 
 
@@ -121,92 +173,30 @@ for(var j=0; j<partDetails.length; j++){
 // var partDetails = partContainer.children;
 
 // console.log(partDetails);
-
-
-// for(var i=0; i<partDetails.length; i++) {
-
-//     // traverse the DOM and get the li element
-//     var supplierNameListItem = partDetails[i].childNodes[1].childNodes[2].childNodes[1];
-//     var supplierNameHTMLString = supplierNameListItem.innerHTML;
-//     supplierNameListItem.innerHTML = supplierNameHTMLString + " " + parts[i].supplier_name;
-//     var partNoListItem = partDetails[i].childNodes[1].childNodes[2].childNodes[3];
-//     var partNoHTMLString = partNoListItem.innerHTML;
-//     partNoListItem.innerHTML = partNoHTMLString + " " + (i+1);
-
-//     //traverse the DOM and get the span element on popout
-//     var detailSpan = partDetails[i].childNodes[3].childNodes[0];
-//     detailSpan.innerHTML = "backlog: " + parts[i].backlog + "<br/>" + "count: " + parts[i].count
-//                            + "<br/>" + "description: "  + parts[i].description + "<br/>" +
-//                            "ETA: " + parts[i].eta_dicv + "<br/>" +
-//                            "planned vehicle quantity: " + parts[i].planned_vehicle_qty + "<br/>" +
-//                            "pmc: " + parts[i].pmc + "<br/>" +
-//                            "quantity: " + parts[i].quantity + "<br/>" +
-//                            "quantity expected: " + parts[i].quantity_expected + "<br/>" +
-//                            "reported on: " + parts[i].reported_on + "<br/>" +
-//                            "short on: " + parts[i].short_on + "<br/>" +
-//                            "shortage reason: " + parts[i].shortage_reason + "<br/>" +
-//                            "truck details: " + parts[i].truck_details + "<br/>" +
-//                            "unloading point: " + parts[i].unloading_point + "<br/>";
-
-//                     }
 }
-
-
 function addItems(list){
-    var container = document.querySelector('div.container');
-    var children = container.children;
 
+
+      var container = document.querySelector('div.container');
+      var children = container.children;
       var itemContainer = children[0].childNodes[1].childNodes[1].childNodes[3].childNodes[1];
-      var child = itemContainer.childNodes;
-      var item = child[1];
-      for( var j=0; j<list.length-1; j++)
+
+      while (itemContainer.firstChild) {
+        itemContainer.removeChild(itemContainer.firstChild);
+      }
+      console.log(list.length);
+      for( var j=0; j<list.length; j++)
       {
           var newItem = item.cloneNode(true);
           itemContainer.appendChild(newItem);
       }
 }
 
-//method to add dates along with the radio buttons
-function updateDates(json){
-
-var dates = Object.keys(json);
-$('#day-1').text(dates[0]);
-$('#day-2').text(dates[1]);
-$('#day-3').text(dates[2]);
-
-
-}
 
 // $('#star').click(function(){
 //   this.setAttribute("src","images/filled_star.png");
 // });
 
-var star = document.getElementById('star');
-star.onclick = function(e){
-var image = e.target;
-this.src = "images/filled_star.png";
-};
 
-// to select a date
-$('#date-selection').click(function(){
-
-  var datesCard = document.getElementById('date-choices');
-  datesCard.style.removeProperty('display');
-  console.log(datesCard.children);
-
-  $('#apply-btn').click(function(){
-      datesCard.setAttribute('style','display: none;');
-
-      var dateSelected = $('input[name=dates]:checked').next().text();
-      console.log(dateSelected);
-      updateDisplay(json, dateSelected);
-
-  });
-
-});
-
-
-
-});
 
 
