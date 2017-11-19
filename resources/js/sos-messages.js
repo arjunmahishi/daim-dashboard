0e51ba90509fb14d31a473a1db96b084ac16f34e
krsnvijay <krsnvijay@gmail.com>
2017-11-19 20:36:09 +0530

    added time to sos cards

--- resources/js/sos-messages.js
+++ resources/js/sos-messages.js
@@ -34,25 +34,25 @@ $(function () {
         for (var i = json.length - 1; i != 0; i--) {
             $("#message").text(json[i].content);
             $("#user").text(json[i].posted_by);
-             if(json[i].media!= undefined)
-           {
-           	openlink(json[i].media);
-           }
+            $("#time").text(jQuery.timeago(json[i].date));
+            if (json[i].media != undefined) {
+                openlink(json[i].media);
+            }
             $("#messageContainer").clone().insertAfter("#messageContainer");
 
         }
+        $("#time").text(jQuery.timeago(json[i].date));
         $("#message").text(json[i].content);
         $("#user").text(json[i].posted_by);
-         if(json[i].media!= undefined)
-           {
-           	openlink(json[i].media);
-           }
+        if (json[i].media != undefined) {
+            openlink(json[i].media);
+        }
 
     }
-    $("#blah").click(function(event){
-  	if(sessionStorage.file!=undefined)
-  	window.open(sessionStorage.file);
-  })
+    $("#blah").click(function (event) {
+        if (sessionStorage.file != undefined)
+            window.open(sessionStorage.file);
+    })
     $("#send").click(function (event) {
         event.preventDefault();
         var text = $("#icon_prefix").val();
@@ -83,16 +83,16 @@ $(function () {
         };
         xhr.send(formData);
     })
- function openlink(link)
-  {
-  	    var a = document.createElement('a');
+
+    function openlink(link) {
+        var a = document.createElement('a');
         var linkText = document.createTextNode("\tClick to view the file");
         a.appendChild(linkText);
         a.title = "\nClick to view the file";
         a.href = link;
-        a.target="_blank";
+        a.target = "_blank";
         document.getElementById("message").append(a);
-  }
- 
+    }
+
 
 })
