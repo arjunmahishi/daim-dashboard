b5b3c6074a6fc0f98054b185535b5c560f523a51
krsnvijay <krsnvijay@gmail.com>
2017-12-03 15:19:44 +0530

    Minor PWA fixes

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,2 +1,19 @@
 // self.addEventListener("fetch", (event)=>{
 //   console.log(event);
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open('app').then(function (cache) {
+            return cache.addAll(
+        ['/',
+          'resources/css/style.css',
+          'resources/js/index.js',
+
+            'https://code.jquery.com/jquery-3.2.1.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js',
+            'https://fonts.googleapis.com/icon?family=Material+Icons'
+
+        ]
+            );
+        })
+    );
+});
